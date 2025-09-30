f0e75d5a78e7596bd9043932a53cb655aec1ecb7
TS01076-LeDinhKhoi <khoildts01076@fpt.edu.vn>
2025-09-30 15:56:59 +0700

    TS01076-LeDinhKhoi-Lab2-LamThem

--- src/main/java/poly/edu/controller/ProductController.java
+++ src/main/java/poly/edu/controller/ProductController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import poly.edu.model.Product;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -14,12 +16,10 @@ public class ProductController {
 
     @GetMapping("/product/form")
     public String form(Model model) {
-        Product p = new Product();
-        p.setName("iPhone 30");
-        p.setPrice(5000.0);
+        Product p = new Product("iPhone 30", 5000.0);
         model.addAttribute("p1", p);
         model.addAttribute("p2", new Product());
-        model.addAttribute("items", getItems());
+        model.addAttribute("items", items);  // dùng list toàn cục
         return "poly/product";
     }
 
@@ -28,13 +28,15 @@ public class ProductController {
         Product defaultP = new Product("iPhone 30", 5000.0);
         model.addAttribute("p1", defaultP);
 
+        items.add(new Product(p.getName(), p.getPrice()));
+
         model.addAttribute("p2", p);
-        model.addAttribute("items", getItems());
+        model.addAttribute("items", items);
         return "poly/product";
     }
 
-    public List<Product> getItems() {
-        return Arrays.asList(new Product("A", 1.0), new Product("B", 12.0));
-    }
+    private List<Product> items = new ArrayList<>(
+            Arrays.asList(new Product("A", 1.0), new Product("B", 12.0))
+    );
 }
 
